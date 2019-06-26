b751fc371e001fa65a153109e264917740a4e30c
xsu <xsu@thoughtworks.com>
2019-06-26 15:37:24 +0800

    finished it. but not get the point.

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -11,22 +11,45 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+        Printer printer = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = new Purchase(new Item[0]);
+
         //when
+        cashRegister.process(purchase);
+
         //then
+        verify(printer).print("");
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
         //given
+        Printer printer = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = mock(Purchase.class);
+        when(purchase.asString()).thenReturn("anything return by purchase");
+
         //when
+        cashRegister.process(purchase);
+
         //then
+        verify(printer).print("anything return by purchase");
     }
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
         //given
+        Printer printer = mock(Printer.class);
+        CashRegister cashRegister = new CashRegister(printer);
+        Purchase purchase = mock(Purchase.class);
+        when(purchase.asString()).thenReturn("anything return by purchase");
+
         //when
+        cashRegister.process(purchase);
+
         //then
+        verify(purchase).asString();
     }
 
 }
